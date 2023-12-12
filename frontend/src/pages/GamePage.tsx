9399e656f563a5048686e1d00b3c4084ad1716f9
Nihar Phansalkar <phansalkarnihar@gmail.com>
2023-12-12 16:18:56 +0000

    added pawn promotion

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -6,27 +6,40 @@ import { Chess } from "chess.js";
 interface pieceMove {
   from: string;
   to: string;
+  promotion: string;
 }
 
 function GamePage({ orientation }) {
-  const chess = useRef(new Chess());
-  const [fen, setFen] = useState(chess.current.fen());
+  const chess = useRef<ChessInstance>(new Chess());
+  const [fen, setFen] = useState<string>(chess.current.fen());
 
   useEffect(() => {
     setFen(chess.current.fen());
-  }, []);
+    if (chess.current.isGameOver()) {
+      console.log("Game over");
+      return;
+    }
+  }, [fen]);
 
-  function dropPiece(sourceSquare, targetSquare) {
+  function dropPiece(
+    sourceSquare: string,
+    targetSquare: string,
+    piece: string,
+  ) {
     const move: pieceMove = {
       from: sourceSquare,
       to: targetSquare,
+      promotion: piece[1].toLowerCase() ?? "q",
     };
+
     try {
       chess.current.move(move);
       setFen(chess.current.fen());
       return true;
     } catch (error) {
+      console.log(error);
       console.log("Invalid move");
+      return false;
     }
   }
 
